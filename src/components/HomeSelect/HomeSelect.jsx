3404373b58ea77c14e80e7eca5277191fb0a8ee6
dhnguyen01 <davidhuongnguyen@gmail.com>
2022-02-23 12:18:19 -0500

    Style changes to navigation cards

--- src/components/HomeSelect/HomeSelect.jsx
+++ src/components/HomeSelect/HomeSelect.jsx
@@ -1,30 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom"
-import { Card, Icon } from "semantic-ui-react"
+import { Card, Icon, Grid } from "semantic-ui-react"
 
 export default function HomeSelect() {
     return (
-        <>
-            <Card.Group itemsPerRow={3} stackable>
-                <Card>
-                    <Link to="/">
-                        <Icon size="large" name="write"></Icon>
-                        Add a Recipe
-                    </Link>
-                </Card>
-                <Card>
-                    <Link to="/">
-                        <Icon size="large" name="book"></Icon>
-                        View My Cookbook
-                    </Link>
-                </Card>
-                <Card>
-                    <Link to="/">
-                        <Icon size="large" name="search"></Icon>
-                        Search Recipes
-                    </Link>
-                </Card>
-            </Card.Group>
-        </>
+        <Grid centered>
+            <Grid.Column style={{ maxWidth: 1250 }}>
+                <Card.Group centered itemsPerRow={3} stackable>
+                    <Card>
+                        <Link to="/">
+                            <Icon size="large" name="write"></Icon>
+                            Add a Recipe
+                        </Link>
+                    </Card>
+                    <Card>
+                        <Link to="/">
+                            <Icon size="large" name="book"></Icon>
+                            View My Cookbook
+                        </Link>
+                    </Card>
+                    <Card>
+                        <Link to="/">
+                            <Icon size="large" name="search"></Icon>
+                            Search Recipes
+                        </Link>
+                    </Card>
+                </Card.Group>
+            </Grid.Column>
+        </Grid>
     )
 }
